7f84f3cd1cf3729b31b9942dbc439a793a865bc7
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2024-03-02 01:02:55 -0500

    Impl() - Text

--- app/src/main/java/com/dioses/jetpackcomposecatalog/MainActivity.kt
+++ app/src/main/java/com/dioses/jetpackcomposecatalog/MainActivity.kt
@@ -3,13 +3,20 @@ package com.dioses.jetpackcomposecatalog
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.dioses.jetpackcomposecatalog.ui.theme.JetpackComposeCatalogTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +29,43 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyText()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeCatalogTheme {
-        Greeting("Android")
+        MyText()
+    }
+}
+
+@Composable
+fun MyText() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "Esto es un ejemplo")
+        Text(text = "Esto es un ejemplo", color = Color.Blue)
+        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.Light)
+        Text(text = "Esto es un ejemplo", fontFamily = FontFamily.Cursive)
+        Text(
+            text = "Esto es un ejemplo", textDecoration = TextDecoration.LineThrough
+        )
+        Text(
+            text = "Esto es un ejemplo", textDecoration = TextDecoration.Underline
+        )
+        Text(
+            text = "Esto es un ejemplo", style = TextStyle(
+                textDecoration = TextDecoration.combine(
+                    listOf(TextDecoration.Underline, TextDecoration.LineThrough)
+                )
+            )
+        )
+        Text(
+            text = "Esto es un ejemplo", fontSize = 30.sp
+        )
     }
 }
