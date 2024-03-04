897a3d450477155ab76828b52343869680361be5
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2024-03-03 19:13:53 -0500

    Impl() - Text Buttons

--- app/src/main/java/com/dioses/jetpackcomposecatalog/MainActivity.kt
+++ app/src/main/java/com/dioses/jetpackcomposecatalog/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -100,6 +101,10 @@ fun MyButton() {
                 disabledContentColor = Color.Yellow
             )
         ) {
+            Text(text = "Hola")
+        }
+
+        TextButton(onClick = { /*TODO*/ }) {
             Text(text = "Hola")
         }
     }
