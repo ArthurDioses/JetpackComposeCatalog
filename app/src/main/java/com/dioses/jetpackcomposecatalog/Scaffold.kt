09215fa76c3c5cb5e92e263bcb10a14ebf510b9a
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2024-03-12 17:12:17 -0500

    Impl() - NavigationBar

--- app/src/main/java/com/dioses/jetpackcomposecatalog/Scaffold.kt
+++ app/src/main/java/com/dioses/jetpackcomposecatalog/Scaffold.kt
@@ -3,10 +3,15 @@ package com.dioses.jetpackcomposecatalog
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Dangerous
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -15,8 +20,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
@@ -27,6 +35,36 @@ import kotlinx.coroutines.launch
  * Created by Arthur Dioses Reto on 8/03/24 at 19:07
  * All rights reserved 2024.
  ****/
+
+@Composable
+fun MyBottomNavigation() {
+    var index by remember {
+        mutableStateOf(0)
+    }
+    NavigationBar(containerColor = Color.Red, contentColor = Color.White) {
+        NavigationBarItem(selected = index == 0, onClick = { index = 0 }, icon = {
+            Icon(
+                imageVector = Icons.Default.Home, contentDescription = "home"
+            )
+        }, label = {
+            Text(text = "Home")
+        })
+        NavigationBarItem(selected = index == 1, onClick = { index = 1 }, icon = {
+            Icon(
+                imageVector = Icons.Default.Favorite, contentDescription = "Fav"
+            )
+        }, label = {
+            Text(text = "FAV")
+        })
+        NavigationBarItem(selected = index == 2, onClick = { index = 2 }, icon = {
+            Icon(
+                imageVector = Icons.Default.Person, contentDescription = "person"
+            )
+        }, label = {
+            Text(text = "Person")
+        })
+    }
+}
 
 @ExperimentalMaterial3Api
 @Composable
@@ -44,6 +82,8 @@ fun ScaffoldExample() {
         }
     }, snackbarHost = {
         SnackbarHost(snackbarHostState)
+    }, bottomBar = {
+        MyBottomNavigation()
     }) {}
 }
 
